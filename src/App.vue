c45b1899c3ef12777061cb2713f8f71a5257838f
chenhao <2212348854@qq.com>
2024-09-26 19:59:19 +0800

    add:增加login

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <router-view></router-view>
+  <router-view />
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const isLoggedIn = ref(false); // 这里可以根据实际情况管理登录状态
+</script>
 
 <style scoped></style>
