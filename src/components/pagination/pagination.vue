699f72e754be2e5a2764c5b24317c4b4087dc9a4
samwong915 <1310997928@qq.com>
2024-10-09 22:43:21 +0800

    feat: build forum page

---
+++ src/components/pagination/pagination.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pagination-container">
+    <el-pagination background layout="prev, pager, next" :total="1000" />
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.pagination-container {
+  display: flex; 
+  justify-content: center; 
+  margin: 20px 0; 
+}
+</style>
