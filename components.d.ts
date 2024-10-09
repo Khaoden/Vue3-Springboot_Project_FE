b00454a929becfe203e1e54d39d42853a91eadae
samwong915 <1310997928@qq.com>
2024-10-09 18:49:12 +0800

    feat: forum page

--- components.d.ts
+++ components.d.ts
@@ -7,13 +7,18 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ElAside: typeof import('element-plus/es')['ElAside']
     ElCol: typeof import('element-plus/es')['ElCol']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElDivider: typeof import('element-plus/es')['ElDivider']
     ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElMain: typeof import('element-plus/es')['ElMain']
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElRow: typeof import('element-plus/es')['ElRow']
     ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     Footer: typeof import('./src/components/footer/footer.vue')['default']
+    ForumList: typeof import('./src/components/forum/forumList.vue')['default']
     Login: typeof import('./src/components/login/login.vue')['default']
     Navbar: typeof import('./src/components/navbar/navbar.vue')['default']
     Post: typeof import('./src/components/post/post.vue')['default']
---
+++ src/components/forum/forumList.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="forum-list">
+        <template class="forum-item" v-for="item in data" :key="item.id">
+            
+        </template>
+    </div>
+</template>
+
+<script setup>
+const data = [
+  {
+    user: {
+      avatar: "null",
+      nickname: "sam1",
+      id: 1,
+      name: "sam wang",
+      email: "12345@qq.com",
+      sex: 1,
+      create_time: "2020-07-10T03:13:22.000000Z",
+      update_time: "2023-04-24T07:50:56.000000Z",
+    },
+    description: "Hello World!",
+    id: 1,
+    causer_id: 1,
+    event: "created",
+    create_time: "2024-10-09T00:40:30.000000Z",
+    update_time: "2024-10-09T00:40:30.000000Z",
+  },
+  {
+    user: {
+      avatar: "null",
+      nickname: "lily1",
+      id: 2,
+      name: "lily",
+      email: "12345@qq.com",
+      sex: 0,
+      create_time: "2020-07-10T03:13:22.000000Z",
+      update_time: "2023-04-24T07:50:56.000000Z",
+    },
+    description: "Hello World!",
+    id: 2,
+    causer_id: 2,
+    event: "created",
+    create_time: "2024-10-09T00:40:30.000000Z",
+    update_time: "2024-10-09T00:40:30.000000Z",
+  },
+  {
+    user: {
+      avatar: "null",
+      nickname: "sam2",
+      id: 3,
+      name: "sam wang",
+      email: "12345@qq.com",
+      sex: 1,
+      create_time: "2020-07-10T03:13:22.000000Z",
+      update_time: "2023-04-24T07:50:56.000000Z",
+    },
+    description: "Hello World!",
+    id: 3,
+    causer_id: 3,
+    event: "created",
+    create_time: "2024-10-09T00:40:30.000000Z",
+    update_time: "2024-10-09T00:40:30.000000Z",
+  },
+  {
+    user: {
+      avatar: "null",
+      nickname: "lily2",
+      id: 4,
+      name: "lily",
+      email: "12345@qq.com",
+      sex: 0,
+      create_time: "2020-07-10T03:13:22.000000Z",
+      update_time: "2023-04-24T07:50:56.000000Z",
+    },
+    description: "Hello World!",
+    id: 4,
+    causer_id: 4,
+    event: "created",
+    create_time: "2024-10-09T00:40:30.000000Z",
+    update_time: "2024-10-09T00:40:30.000000Z",
+  },
+  {
+    user: {
+      avatar: "null",
+      nickname: "sam3",
+      id: 5,
+      name: "sam wang",
+      email: "12345@qq.com",
+      sex: 1,
+      create_time: "2020-07-10T03:13:22.000000Z",
+      update_time: "2023-04-24T07:50:56.000000Z",
+    },
+    description: "Hello World!",
+    id: 5,
+    causer_id: 5,
+    event: "created",
+    create_time: "2024-10-09T00:40:30.000000Z",
+    update_time: "2024-10-09T00:40:30.000000Z",
+  },
+];
+</script>
+
+<style lang="scss" scoped>
+
+</style>
