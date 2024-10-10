4760d56de1c5276ae3c039c470c5752f266ec876
samwong915 <1310997928@qq.com>
2024-10-10 23:22:50 +0800

    feat: 微调论坛样式

--- src/components/forum/forumAside.vue
+++ src/components/forum/forumAside.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="function-container">
     <div class="text-area"></div>
+    <el-divider class="divider" />
     <div class="button-container">
       <el-button type="primary" plain>发帖交流</el-button>
       <el-button type="success" plain>加入益行</el-button>
       <el-button type="warning" plain>助力益行</el-button>
     </div>
+    <el-divider class="divider" />
   </div>
 </template>
 
@@ -33,13 +35,16 @@ const projects = [
     status: 3,
   },
 ];
+
+// TODO:
+// 1.按钮上方做一个3D效果
+// 2.按钮下方塞一点项目item
 </script>
 
 <style lang="scss" scoped>
 .function-container {
   .text-area {
     height: 200px;
-    background-color: rgba(0, 0, 0, 0.1);
     line-height: 50px;
     font-weight: 800;
   }
@@ -49,7 +54,7 @@ const projects = [
     justify-content: center;
     align-items: center;
     height: 50px;
-    background-color: #f2f3f5;
   }
 }
 </style>
+
